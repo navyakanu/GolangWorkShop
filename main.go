d7a9db33f8e0c12944bf7b31c20d29180445f5e6
NavyaB <navya-balikeri@external.mckinsey.com>
2016-07-22 12:42:02 +0530

    Navya - Some different variable declarations and printing

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package exper
+package main
 
 import (
 	"fmt"
@@ -6,6 +6,18 @@ import (
 
 func main() {
 
-	fmt.Println("Main.go executing")
+	var variable string="Golang Workshop"
+	fmt.Println(variable)
+	fmt.Println("Main.go executing\n")
+	x:=20
+	fmt.Println(x)
+	y:=variableprint()
+	fmt.Println(y)
+}
 
+
+
+func variableprint() int {
+
+	return 2
 }
